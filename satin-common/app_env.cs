b9716a4cf5a12202b2440324dabefd73f23080c1
Pradeep CK <CoreNexus09@outlook.com>
2025-08-18 12:33:03 +1000

    home_dir now refers to the folder from which the .exe is executed. MQ text files were previously saved in the TMP folder; they are now saved in the MQ folder.

--- satin-common/app_env.cs
+++ satin-common/app_env.cs
@@ -7,7 +7,7 @@ namespace satin_common
     {
         public static readonly string project_name;
         public static readonly string exe_file;
-        public static readonly string root_dir;
+        public static readonly string home_dir;
         public static readonly string log_dir;
         public static readonly string tmp_dir;
         public static readonly string mq_dir;
@@ -22,24 +22,25 @@ namespace satin_common
             try
             {
                 project_version = Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "0.0.0.0";
-
                 exe_file = Process.GetCurrentProcess().MainModule?.FileName ?? throw new InvalidOperationException("Could not determine exe name"); ;
-                // exe_file = "c:\\satin\\satin-mqtt-send.exe";
-                // root_dir = Path.GetDirectoryName(exe_file) ?? throw new InvalidOperationException("Could not determine root directory");
-                root_dir = "c:\\satin\\";
                 project_name = Path.GetFileNameWithoutExtension(exe_file);
 
-                log_dir = Path.Combine(root_dir, "log");
-                tmp_dir = Path.Combine(root_dir, "tmp");
-                cert_dir = Path.Combine(root_dir, "cert");
-                mq_dir = Path.Combine(root_dir, "mq");
+                if (Debugger.IsAttached) { home_dir = "c:\\satin\\"; } 
+                else {
+                    home_dir = Path.GetDirectoryName(exe_file) ?? throw new InvalidOperationException("Could not determine root directory");
+                }                
+                
+                log_dir = Path.Combine(home_dir, "log");
+                tmp_dir = Path.Combine(home_dir, "tmp");
+                cert_dir = Path.Combine(home_dir, "cert");
+                mq_dir = Path.Combine(home_dir, "mq");
 
                 Directory.CreateDirectory(log_dir);
                 Directory.CreateDirectory(tmp_dir);
                 Directory.CreateDirectory(cert_dir);
                 Directory.CreateDirectory(mq_dir);
 
-                ini_file = Path.Combine(root_dir, $"{project_name}.ini");
+                ini_file = Path.Combine(home_dir, $"{project_name}.ini");
 
                 string timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
                 log_file = Path.Combine(log_dir, $"{project_name}_{timestamp}.log");
